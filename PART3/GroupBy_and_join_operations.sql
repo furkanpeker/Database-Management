05560e4d3285ede5fdaf09a14f367e6a3ab46ef2
Furkan PEKER <73311880+furkanpeker@users.noreply.github.com>
2021-12-28 14:27:40 +0300

    Add files via upload

---
+++ PART3/GroupBy_and_join_operations.sql
@@ -0,0 +1,52 @@
+﻿/* GROUP BY Formula: Select column_name from table_name where condition group by column_name having condition (optional) order by column_name */
+
+select gorev, avg(maas) as ortalama from personel group by gorev /* its database is not exist in this machine! */
+
+select kütüphane_no avg(datediff(day, emanet_tarihi, teslim_tarihi) as ortSure from emanet group by kütüphane_no
+
+select  bolum, gorev, max(maas) , min(maas) from personel group by bolum, gorev  order by bolum 
+
+/* Northwind ---GROUP BY--- examples */
+select count(CustomerID) as musteri_sayisi, Country as ulke from Customers group by Country
+select count(CustomerID) as musteri_sayisi, Country as ulke from Customers group by Country order by count(*) desc
+select count(CustomerID) as musteri_sayisi, Country as ulke from Customers group by Country having count(CustomerID)>5 order by count(*) desc
+
+select ProductID, sum(Quantity) as TOTAL_ORDER_AMOUNT from [Order Details] group by ProductID having sum(Quantity) > 1300 
+
+select City, Title, count(EmployeeID) as 'NUM OF EMPLOYEES' from Employees where City='London' group by City,Title
+select Country, City, Title, count(EmployeeID) as 'Num of Employees' from Employees group by Country,City,Title having count(*)>1
+
+
+
+
+/* JOINNING WITH WHERE */
+/* FORMULA: SELECT NICKNAME.COULMNNAME, NICKNAME2.COULMNNAME ... FROM TABLE_NAME AS NICKNAME, TABLE_NAME2 AS NICKNAME2 WHERE EQUVALENT */
+/*EXAMPLE:*/
+SELECT O.OrderID, C.ContactName, DATEPART(YEAR,O.OrderDate) OrderYear from Orders AS O, Customers AS C 
+WHERE O.CustomerID = C.CustomerID 
+ORDER BY O.OrderDate DESC
+
+/* JOINNING WITH JOIN */
+/* JOIN Formula: select * from Table1_name AS nickName JOIN Table2_name AS nickName ON equvalent */
+
+/* Northwind ---JOIN--- examples */
+/*INNER JOIN: ONLY COMMON RECORDS/DATA WILL BE LIST/APPEAR IN THE RESULT TABLE!*/
+
+SELECT O.OrderID, C.ContactName, DATEPART(YEAR,O.OrderDate) OrderYear from Orders AS O JOIN Customers AS C 
+ON O.CustomerID = C.CustomerID 
+ORDER BY O.OrderDate DESC
+
+SELECT * from Orders AS O JOIN Customers AS C 
+ON O.CustomerID = C.CustomerID 
+ORDER BY O.OrderDate DESC
+
+SELECT * from  Customers AS C JOIN Orders AS O 
+ON O.CustomerID = C.CustomerID 
+ORDER BY O.OrderDate DESC
+
+/* LEFT OUTER JOIN */
+
+
+
+
+
